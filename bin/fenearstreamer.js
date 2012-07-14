5ba4f003387007640514a0654078286791c758d1
Lee Olayvar <leeolayvar@gmail.com>
2012-07-14 16:14:42 -0400

    Added a comment explaining the use of JavaScript vs CoffeeScript for the bin file. The reason for this is that i'm stupid and i almost converted it to Coffee.. *again*.

--- bin/fenearstreamer.js
+++ bin/fenearstreamer.js
@@ -2,6 +2,11 @@
 //
 // bin/fenearstreamer.js
 //
+// The main fenear streamer entry point.
+//
+// Note that this file is in JavaScript because CoffeeScript is currently
+// unable to properly add hashbangs as this file has.
+//
 // Copyright (c) 2012 Lee Olayvar <leeolayvar@gmail.com>
 // MIT Licensed
 //
